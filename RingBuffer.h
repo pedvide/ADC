c74f8fe1b91ee957aafd174f09fef5946f10e054
pedvide <pedvide@gmail.com>
2014-06-15 14:05:40 +0200

    license and documentation

--- RingBuffer.h
+++ RingBuffer.h
@@ -1,3 +1,28 @@
+ /* Teensy 3.x ADC library
+ * https://github.com/pedvide/ADC
+ * Copyright (c) 2014 Pedro Villanueva
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef RINGBUFFER_H
 #define RINGBUFFER_H
 
@@ -17,12 +42,6 @@ class RingBuffer
     public:
         //! Default constructor, buffer has a size DEFAULT_BUFFER_SIZE
         RingBuffer();
-
-        //! Buffer has a size buffer_size
-        /**
-        * \param buffer_size must be a power of two!
-        */
-        //RingBuffer(int buffer_size);
 
         /** Default destructor */
         virtual ~RingBuffer();
