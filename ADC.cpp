a3cef196120fea4ff397dee7be597f2752b5327b
pedvide <pedvide@gmail.com>
2016-12-09 23:55:33 +0100

    fix another error with merge

--- ADC.cpp
+++ ADC.cpp
@@ -118,7 +118,6 @@ const uint8_t ADC::channel2sc1aADC1[]= { // new version, gives directly the sc1a
         {A10, 0}
     };
 #elif defined(ADC_TEENSY_3_5) || defined(ADC_TEENSY_3_6) // Teensy 3.6// Teensy 3.5
->>>>>>> refs/remotes/origin/dev
     const ADC_Module::ADC_NLIST ADC::diff_table_ADC0[]= {
         {A10, 3}
     };
