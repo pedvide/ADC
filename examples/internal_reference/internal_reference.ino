89f28acfaab89ecbc817496e16aee8b8e98d4d09
pedvide <pedvide@gmail.com>
2017-10-16 23:07:31 +0200

    works with Teensy 3.0 too
    change to adc-> methods instead of adc->adcX, so it works for boards
    with one ADC (Teensy LC doesn't have a VREF though)
    change resolution and speeds on ADC1 if it exists to detect smaller
    changes

--- examples/internal_reference/internal_reference.ino
+++ examples/internal_reference/internal_reference.ino
@@ -5,6 +5,7 @@
 *   change voltage_target accordingly and run the program. The optimum trim value is printed at the end.
 *   The bisection algorithm prints some diagnostics while running.
 *   Additionally, the bandgap voltage is printed too, it should lie between 0.97 V and 1.03 V, with 1.00 V being the typical value.
+*   Because of noise, the trim value is not accurate to 1 step, it fluctuates +- 1 or 2 steps.
 */
 
 #include <ADC.h>
@@ -15,9 +16,9 @@ ADC* adc = new ADC();
 //! change this value to your real input value, measured between AGND and 3.3V
 float voltage_target = 3.3;
 
-//! change the TOL (tolerance, minimum difference between voltage and target that matters)
-//! to refine even more, but not to less than 0.001 or 0.002
-const float TOL = 0.002;
+//! change the TOL (tolerance, minimum difference  in mV between voltage and target that matters)
+//! to refine even more, but not to less than 0.5 mV
+const float TOL = 1.0;
 // Maximum iterations of the algorithm, no need to change it.
 const uint8_t MAX_ITER = 100;
 
@@ -26,13 +27,13 @@ const uint8_t MAX_ITER = 100;
 float get_voltage(uint8_t trim) {
     VREF::trim(trim);
     VREF::waitUntilStable();
-    return 1.20/adc->adc1->analogRead(ADC_INTERNAL_SOURCE::VREF_OUT)*(adc->getMaxValue(ADC_1));
+    return 1.20/adc->analogRead(ADC_INTERNAL_SOURCE::VREF_OUT)*(adc->getMaxValue(ADC_0));
 }
 
 // Simple bisection method to get the optimum trim value
+// This method is not the bests for noisy functions...
 // Electrical datasheet: "The Voltage Reference (VREF) is intended to supply an accurate voltage output
 // that can be trimmed in 0.5 mV steps."
-// But it seems that the resolution of the ADC doesn't allow such fine tuning, 1 mV is the minimum
 int8_t optimumTrimValue(float voltage_target) {
     // https://en.wikipedia.org/wiki/Bisection_method#Algorithm
     // INPUT: Function f, endpoint values a, b, tolerance TOL, maximum iterations NMAX
@@ -64,13 +65,13 @@ int8_t optimumTrimValue(float voltage_target) {
 
     while (niter < MAX_ITER) {
         midpoint = (a + b)/2;
-        cur_diff = get_voltage(midpoint) - voltage_target;
+        cur_diff = (get_voltage(midpoint) - voltage_target)*1000;
 
         Serial.print(niter, DEC); Serial.print(",\t");
         Serial.print(a, DEC); Serial.print(",\t");
         Serial.print(b, DEC); Serial.print(",\t");
         Serial.print(midpoint, DEC); Serial.print(",\t\t");
-        Serial.println(cur_diff*1000, 2);
+        Serial.println(cur_diff, 2);
 
         if (abs(cur_diff) <= TOL || b-a < 1) {
             return midpoint;
@@ -91,10 +92,17 @@ int8_t optimumTrimValue(float voltage_target) {
 void setup() {
     Serial.begin(9600);
 
+    // Best measurement conditions
     adc->setAveraging(32);
     adc->setResolution(16);
     adc->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_LOW_SPEED);
     adc->setSamplingSpeed(ADC_SAMPLING_SPEED::VERY_LOW_SPEED);
+    #if ADC_NUM_ADCS>1
+    adc->setAveraging(32, ADC_1);
+    adc->setResolution(16, ADC_1);
+    adc->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_LOW_SPEED, ADC_1);
+    adc->setSamplingSpeed(ADC_SAMPLING_SPEED::VERY_LOW_SPEED, ADC_1);
+    #endif // ADC_NUM_ADCS
 
     delay(2000);
 
@@ -105,41 +113,64 @@ void setup() {
     VREF::waitUntilStable();
 
     Serial.print("VREF value: ");
-    Serial.print(1.20/adc->adc1->analogRead(ADC_INTERNAL_SOURCE::VREF_OUT)*adc->getMaxValue(ADC_1), 5);
+    Serial.print(1.20/adc->analogRead(ADC_INTERNAL_SOURCE::VREF_OUT)*adc->getMaxValue(ADC_0), 5);
     Serial.println(" V.");
 
 
     Serial.print("Bandgap value: ");
-    Serial.print(3.3*adc->adc0->analogRead(ADC_INTERNAL_SOURCE::BANDGAP)/adc->getMaxValue(ADC_0), 5);
+    Serial.print(3.3*adc->analogRead(ADC_INTERNAL_SOURCE::BANDGAP)/adc->getMaxValue(ADC_0), 5);
     Serial.println(" V.");
 
     VREF::stop();
 }
 
 
-void loop() {}
+void loop() {
+}
 
-/* My output with a Teensy 3.6 connected to my laptop's USB.
+/*
+
+My output with a Teensy 3.6 connected to my laptop's USB.
 
 With a cheap multimeter I measured voltage_target = 3.29 V, the results are:
 
 niter,	a,	b,	midpoint,	diff (mV)
-0,		0,	63,	31,			27.84
-1,		31,	63,	47,			10.00
-2,		47,	63,	55,			1.15
-Optimal trim value: 55
-VREF value: 3.29115 V.
-Bandgap value: 1.00271 V.
+0,	    0,	63,	31,	    	38.62
+1,  	31,	63,	47,	    	17.06
+2,  	47,	63,	55,	    	6.81
+3,  	55,	63,	59,	    	1.42
+4,  	59,	63,	61,	    	-1.74
+5,  	59,	61,	60,	    	-0.50
+Optimal trim value: 60
+VREF value: 3.28977 V.
+Bandgap value: 1.00256 V.
 
 
 Using voltage_target = 3.3, which should be true:
 
 niter,	a,	b,	midpoint,	diff (mV)
-0,	    0,	63,	31,	    	26.83
-1,  	31,	63,	47,	    	0.00
-Optimal trim value: 47
-VREF value: 3.30000 V.
-Bandgap value: 1.00282 V.
+0,	    0,	63,	31,	    	28.48
+1,	    31,	63,	47,	    	7.20
+2,  	47,	63,	55,	    	-3.33
+3,	    47,	55,	51,	    	1.79
+4,	    51,	55,	53,	    	-0.84
+Optimal trim value: 53
+VREF value: 3.29874 V.
+Bandgap value: 1.00317 V.
+
+
+
+My output with a Teensy 3.0 connected to my laptop's USB.
+voltage_target = 3.22 V, the results are:
+
+niter,	a,	b,	midpoint,	diff (mV)
+0,  	0,	63,	31,		    37.34
+1,  	31,	63,	47,	    	16.56
+2,	    47,	63,	55,	    	5.54
+3,	    55,	63,	59,	    	-0.00
+Optimal trim value: 59
+VREF value: 3.21947 V.
+Bandgap value: 1.01978 V.
 
 
 */
