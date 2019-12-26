70ee43e47228198ba1b667caf806e46f9158bbb2
Pedro Villanueva <pedvide@gmail.com>
2019-12-26 20:35:30 +0100

    add license, change guarding defines, and update info

--- atomic.h
+++ atomic.h
@@ -1,5 +1,30 @@
-#ifndef ATOMIC_H
-#define ATOMIC_H
+/* Teensy 4, 3.x, LC ADC library
+ * https://github.com/pedvide/ADC
+ * Copyright (c) 2019 Pedro Villanueva
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef ADC_ATOMIC_H
+#define ADC_ATOMIC_H
 
 /*  int __builtin_ctz (unsigned int x):
     Returns the number of trailing 0-bits in x, 
@@ -188,4 +213,4 @@ namespace atomic
 
 }
 
-#endif // ATOMIC_H
+#endif // ADC_ATOMIC_H
