afb2ffcd75c1d747ccef66e37e08a8c290b9f828
Pedro Villanueva <pedvide@gmail.com>
2022-12-04 16:04:55 +0100

    get asm for debug

---
+++ objdump.py
@@ -0,0 +1,12 @@
+"""Run objdump on the .elf file to get the assembly for inspection. Only runs in debug mode"""
+
+Import("env")
+
+env.AddPostAction(
+    "$BUILD_DIR/${PROGNAME}.elf",
+    env.VerboseAction(
+        "arm-none-eabi-objdump -d -S $BUILD_DIR/${PROGNAME}.elf > $BUILD_DIR/${PROGNAME}.s",
+        "Building ${PROGNAME}.s with objdump"
+    )
+)
+    
