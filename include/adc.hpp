2a3234edc3c6ce6aecc27099f0a5a23dc7928761
Pedro Villanueva <pedvide@gmail.com>
2022-12-04 16:05:02 +0100

    simplify

--- include/adc.hpp
+++ include/adc.hpp
@@ -1,11 +1,8 @@
 #include "adc_module.hpp"
 
-namespace adc
-{
-    struct adc_t
-    {
-        using adc0 = adc_module_t<0>;
-        using adc1 = adc_module_t<1>;
-    };
+namespace adc {
+
+using adc0 = adc_module_t<0>;
+using adc1 = adc_module_t<1>;
 
 }; // namespace adc
