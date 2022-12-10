f767ef54b752a96853775543600815b3b1562b23
Pedro Villanueva <pedvide@gmail.com>
2022-12-10 19:14:40 +0100

    add missing pin and fix mapping

--- include/teensy3x.hpp
+++ include/teensy3x.hpp
@@ -64,6 +64,8 @@ template <> struct pin_info_t<board_t::TEENSY_3_2, 0> {
     A10 = 0,
     A11 = 19,
     A12 = 3,
+    // A13 not connected as a single-ended pin
+    A14 = 23,
     TEMP_SENSOR = 26,
     VREF_OUT = 22,
     BANDGAP = 27,
@@ -71,14 +73,14 @@ template <> struct pin_info_t<board_t::TEENSY_3_2, 0> {
     VREFL = 30
   };
 
-  static constexpr pin_t pins[] = {pin_t::A0, pin_t::A1, pin_t::A2,  pin_t::A3,
-                                   pin_t::A4, pin_t::A5, pin_t::A6,  pin_t::A7,
-                                   pin_t::A8, pin_t::A9, pin_t::A10, pin_t::A11,
-                                   pin_t::A12};
+  static constexpr pin_t pins[] = {
+      pin_t::A0,  pin_t::A1,  pin_t::A2,  pin_t::A3, pin_t::A4,
+      pin_t::A5,  pin_t::A6,  pin_t::A7,  pin_t::A8, pin_t::A9,
+      pin_t::A10, pin_t::A11, pin_t::A12, pin_t::A14};
 
-  static constexpr uint8_t pin2int[] = {10, 255, 255, 255, 9,   0,  6,
-                                        7,  2,   3,   255, 255, 5,  4,
-                                        1,  8,   255, 255, 255, 11, 255};
+  static constexpr uint8_t pin2int[] = {10,  255, 255, 12,  9,   0,   6,   7,
+                                        2,   3,   255, 255, 5,   4,   1,   8,
+                                        255, 255, 255, 11,  255, 255, 255, 14};
 };
 // make linker happy
 constexpr uint8_t pin_info_t<board_t::TEENSY_3_2, 0>::pin2int[];
